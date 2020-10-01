a32a51717cf2de6c6e6cd2860c9bbc796011eb24
BeyoncesHomeboy <18110110+BeyoncesHomeboy@users.noreply.github.com>
2020-10-01 16:06:30 -0500

    first

---
+++ sculp-gcloud-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3366b848c2c45403a03916b39e3fa6fd'),
+    },
+  },
+});
---
+++ sculp-gcloud-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ecb38355-971e-464d-a5e1-b0d9c69ea3b7'
+};
